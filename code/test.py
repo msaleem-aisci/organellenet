94bb73835568cfd2bdb8e456ac350a43d9981676
Muhammad Saleem <msaleem.aisci@gmail.com>
2026-09-18 01:26:04 +0500

    train_dataset.crops

--- code/test.py
+++ code/test.py
@@ -44,22 +44,21 @@ def main():
 
     ZARR_MAP = build_zarr_map(root_dir)
 
+  
+    rfs_weights_path = os.path.join(cfg.path.jsons, cfg.rfs_weights)
+   
+    with open(split_paths['train_crops'], 'r') as f:
+        train_crops_json = json.load(f)
+
+    with open(rfs_weights_path, 'r') as f:
+        rfs_weights_json = json.load(f)
+
     train_dataset = MISO2DDataset(
         crops_json_path = split_paths['train_crops'], 
         zarr_map = ZARR_MAP,
     )
 
-    rfs_weights_path = os.path.join(cfg.path.jsons, cfg.rfs_weights)
- 
-   
-    with open(split_paths['train_crops'], 'r') as f:
-        train_crops_json = json.load(f)
-
-    
-    with open(rfs_weights_path, 'r') as f:
-        rfs_weights_json = json.load(f)
-
-    print(train_crops_json)
+    print(train_dataset.crops)
     sys.exit(0)
     train_sampler = create_rfs_sampler()
 
