3d799a4a1235cf3d3dfeafdb2690de0c6a259c2a
Muhammad Saleem <msaleem.aisci@gmail.com>
2026-09-19 18:36:35 +0500

    class names

--- code/train.py
+++ code/train.py
@@ -73,8 +73,9 @@ def main():
     with open(rfs_weights_path, 'r') as f:
         rfs_weights_json = json.load(f)
 
+    print(cfg.semantic_map)
 
-
+    sys.exit(0)
     train_dataset = MISO2DDataset(
         crops_json_path = split_paths['train_crops'], 
         zarr_map = ZARR_MAP,
