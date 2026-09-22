30206b04dc583f9ddc0971c0b520f7f3c2e9470d
Muhammad Saleem <msaleem.aisci@gmail.com>
2026-09-22 18:21:12 +0500

    sd

--- code/train.py
+++ code/train.py
@@ -96,7 +96,7 @@ def main():
     print(f"Class Mapper: {ctc.lass_mapper()}")
 
 
-    
+    sys.exist(0)
     rfs_weights_path = os.path.join(cfg.path.jsons, cfg.rfs_weights)
    
     with open(rfs_weights_path, 'r') as f:
